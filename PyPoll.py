c163fc836edf1a429885bbcbb9e53b5d08dc56cb
rbriggs13 <ryanbriggs101@yahoo.com>
2021-09-10 16:05:35 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,16 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 #Creating a filename variable and open it in order to write in it
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#variable to count all the votes
+total_votes = 0
+#list for the candidates
+candidate_options = []
+#dictionary to hold the votes for each candidate
+candidate_votes = {}
+#variables used to determine the winner
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 
 #open the elections results and read the file
@@ -22,8 +32,46 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
 
     headers = next(file_reader)
-    print(headers)
-#Perform the analysis
-    
 
+
+    #Count each vote in the csv file
+    for row in file_reader:
+        total_votes += 1
+        
+        #adding candidate names to the list
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            candidate_options.append(candidate_name)
+
+            #starting the track the candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        candidate_votes[candidate_name] +=1
+
+#calculate vote percentage and print the results
+for candidate in candidate_options:
+    votes = candidate_votes[candidate]
+    vote_percent = float(votes)/float(total_votes) * 100
+
+    #setting the winning candidate
+    if votes > winning_count and vote_percent > winning_percentage:
+        winning_count = votes
+        winning_percentage = vote_percent
+        winning_candidate = candidate
+
+    print(f'{candidate}: {vote_percent:.1f}% ({votes:,})\n')
+
+
+#print the winning candidate
+winning_summary = (
+    f'-----------------------\n'
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_count:,}\n'
+    f'Winning Percentage: {winning_percentage:.1f}%\n'
+    f'-----------------------\n'
+)
+
+print(winning_summary)
 
